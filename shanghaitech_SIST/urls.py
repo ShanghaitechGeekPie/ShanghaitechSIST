3d39c8874f4442100fa3e77af86921647823df0a
eastpiger <lvwt@shanghaitech.edu.cn>
2015-12-10 17:02:33 +0800

    debug

--- shanghaitech_SIST/urls.py
+++ shanghaitech_SIST/urls.py
@@ -21,6 +21,5 @@ from django.contrib import admin
 
 urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^', include('cms.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
