a020780c6718aa2842deb44571fcf20c5376b4c4
eastpiger <lvwt@shanghaitech.edu.cn>
2016-01-27 18:30:35 +0800

    mobile mode

--- static/js/common.js
+++ static/js/common.js
@@ -2,4 +2,7 @@ $(function(){
 	$(".clickbox").click(function(){
 		location.href = $(this).attr("href")
 	})
+	$("#top-menu-btn").click(function(){
+		$("body").toggleClass("mobile-menu-enabled");
+	})
 })
